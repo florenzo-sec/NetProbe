9798f37ec109600855ffc4d713f2d7d66b3132da
florenzo-sec <lorenzo.cybersec@gmail.com>
2026-03-05 23:29:22 +0100

    Optimizations + Terminal Colors(Colorama)

--- netprobe.py
+++ netprobe.py
@@ -2,14 +2,23 @@ import socket
 from args import parse_ports,args
 import colorama
 
-for p in parse_ports(args()):
-    
+colorama.init(autoreset=True) #set terminal colors autoreset
+
+arguments = args() # grab arguments from CLI
+
+print(f"Scanning {arguments['host']}\n")
+
+for p in parse_ports(arguments):
+
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #socket generation
     s.settimeout(2) # timeout so the scanner doesn't wait infinitly
-    
-    if s.connect_ex((args()["host"],p))!=0: #port is open=0, else >0
-        print("Porta chiusa")
+
+    status = s.connect_ex((arguments["host"],p))
+
+    if status!=0: #port is open=0, else >0
+        print(f"[{colorama.Fore.RED}CLOSED{colorama.Fore.RESET}]\t{p}")
     else:
-        print("Porta aperta")
-        
+        print(f"[{colorama.Fore.GREEN}OPEN{colorama.Fore.RESET}]\t{p}")
+    
+    s.close()
             
