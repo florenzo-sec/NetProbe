07a2b3170f1cd3eeece90835ec6f285eca2351dd
florenzo-sec <lorenzo.cybersec@gmail.com>
2026-03-06 20:50:25 +0100

    Threading added

--- netprobe.py
+++ netprobe.py
@@ -1,15 +1,11 @@
 import socket
 from args import parse_ports,args
 import colorama
+import threading
 
 colorama.init(autoreset=True) #set terminal colors autoreset
 
-arguments = args() # grab arguments from CLI
-
-print(f"Scanning {arguments['host']}\n")
-
-for p in parse_ports(arguments):
-
+def port_scan(host,p):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #socket generation
     s.settimeout(2) # timeout so the scanner doesn't wait infinitly
 
@@ -21,4 +17,18 @@ for p in parse_ports(arguments):
         print(f"[{colorama.Fore.GREEN}OPEN{colorama.Fore.RESET}]\t{p}")
     
     s.close()
+
+arguments = args() # grab arguments from CLI
+
+print(f"Scanning {arguments['host']}\n")
+
+threads = []
+
+for p in parse_ports(arguments):
+    t = threading.Thread(target=port_scan,args=(arguments['host'],p))
+    threads.append(t)
+    t.start()
+    
+for t in threads:
+    t.join()
             
